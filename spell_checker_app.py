4b1a1c6efb69e41b0e560cdb02c4eb322ce087e9
KHADIJA SABIR <awaissabir107@gmail.com>
2025-09-13 20:33:38 +0500

    first commit

---
+++ spell_checker_app.py
@@ -0,0 +1,34 @@
+# Step 1 : Importing the required library
+from spellchecker import SpellChecker
+
+# Step 2 : Creating the app class
+class SpellCheckerApp():
+    def __init__(self):
+        self.spell=SpellChecker()
+
+    def correct_text(self,text):
+        words=text.split()
+        corrected_words=[]
+
+        for word in words:
+            corrected_word = self.spell.correction(word)  
+            if corrected_word != word.lower():
+                print(f"Correcting {word} to {corrected_word}")
+                corrected_words.append(corrected_word)
+        # returning the corrected text        
+        return " ".join(corrected_words)          
+
+    def run(self):
+        print("-----SPELL CHECKER-----") 
+
+        while True:
+            text = input("Enter the text (or type exit to quit) : ")
+            if text.lower()=="exit":
+                print("Closing the program.")
+                break
+            corrected_text=self.correct_text(text)
+            print(f"Corrected Text : {corrected_text}")
+
+# Running the main program              
+if __name__=="__main__":
+    SpellCheckerApp().run()              
